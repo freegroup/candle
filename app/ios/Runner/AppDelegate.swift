b2daba8febd86ac222304dac0765db5994179357
man from earth <1155039+freegroup@users.noreply.github.com>
2024-03-04 00:25:03 +0100

    ..

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import receive_sharing_intent
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,41 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    print("application inital.......")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    print("Application called to open URL: \(url)")
+    
+    // Ensure the URL is a file URL and that the file has the correct extension
+    guard url.isFileURL && url.pathExtension == "candle" else {
+      return false
+    }
+
+    // Start accessing a security-scoped resource.
+    guard url.startAccessingSecurityScopedResource() else {
+      // Handle the failure here if you can't access the resource.
+      return false
+    }
+
+    // Your app should read the file and process the contents.
+    do {
+      let fileData = try Data(contentsOf: url)
+      // Process the file data (e.g., pass to Flutter/Dart side for handling)
+    } catch {
+      print("Failed to read the file: \(error)")
+      url.stopAccessingSecurityScopedResource()
+      return false
+    }
+
+    // Stop accessing the security-scoped resource
+    url.stopAccessingSecurityScopedResource()
+
+    return true
+  }
+
+  // Add any additional methods needed to process the file data as appropriate for your app.
 }
--- app/ios/Share Extension/ShareViewController.swift
+++ app/ios/Share Extension/ShareViewController.swift
@@ -1,30 +1,18 @@
-//
-//  ShareViewController.swift
-//  Share Extension
-//
-//  Created by Herz, Andreas on 03.03.24.
-//
+import receive_sharing_intent
 
-import UIKit
-import Social
-
-class ShareViewController: SLComposeServiceViewController {
-
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
-
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+class ShareViewController: RSIShareViewController {
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    // Use this method to return false if you don't want to redirect to host app automatically.
+    // Default is true
+    override func shouldAutoRedirect() -> Bool {
+        print("shouldAutoRedirect")
+        return false
     }
-
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    
+    // Use this to change label of Post button
+    override func presentationAnimationDidFinish() {
+        print("presentationAnimationDidFinish")
+        super.presentationAnimationDidFinish()
+        navigationController?.navigationBar.topItem?.rightBarButtonItem?.title = "Send"
     }
-
 }
